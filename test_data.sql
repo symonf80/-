f9abfce3fe39161653e07a91af2f207bb81a60a0
symonf80 <94631730+symonf80@users.noreply.github.com>
2022-08-23 21:43:39 +0300

    Create test_data.sql

---
+++ test_data.sql
@@ -0,0 +1,8 @@
+insert into PERSONS (name, surname, age, phone_number, city_of_living)
+values ('Саша', 'Александров', 30, 12345, 'MOSCOW');
+
+insert into PERSONS(name, surname, age, phone_number, city_of_living)
+values ('Паша', 'Пашин', 24, 67891, 'SOCHI');
+
+insert into PERSONS(name, surname, age, phone_number, city_of_living)
+values ('Петя', 'Петин', 44, 645378, 'MOSCOW');
